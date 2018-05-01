44ba0021ee2b255e90e4efe3aac5d60b48430366
R3dWulf <theakaiokami@gmail.com>
2018-05-01 10:53:43 -0400

    Main button will now turn green to show which photo is main; auto image crop on upload

--- DatingAPP.API/Controllers/PhotosController.cs
+++ DatingAPP.API/Controllers/PhotosController.cs
@@ -77,7 +77,8 @@ namespace DatingAPP.API.Controllers
                 {
                     var uploadParams = new ImageUploadParams()
                     {
-                        File = new FileDescription(file.Name, stream)
+                        File = new FileDescription(file.Name, stream),
+                        Transformation = new Transformation().Width(500).Height(500).Crop("fill").Gravity("face")
                     };
 
                     uploadResult = _cloudinary.Upload(uploadParams);
--- DatingApp.SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp.SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -5,6 +5,7 @@ import { environment } from '../../../environments/environment';
 import { AuthService } from '../../_services/auth.service';
 import { UserService } from '../../_services/user.service';
 import { AlertifyService } from '../../_services/alertify.service';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-photo-editor',
@@ -16,6 +17,7 @@ export class PhotoEditorComponent implements OnInit {
   uploader: FileUploader;
   hasBaseDropZone = false;
   baseUrl = environment.apiUrl;
+  currentMain: Photo;
 
   constructor(private authService: AuthService, private userService: UserService, private alertify: AlertifyService) { }
 
@@ -58,7 +60,9 @@ export class PhotoEditorComponent implements OnInit {
     this.userService
       .setMainPhoto(this.authService.decodedToken.nameid, photo.id)
       .subscribe(() => {
-        console.log('Successfully set photo as main');
+        this.currentMain = _.findWhere(this.photos, {isMain: true});
+        this.currentMain.isMain = false;
+        photo.isMain = true;
       }, error => {
         this.alertify.error(error);
       });
