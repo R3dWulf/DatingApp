a90cbfe0d7cdf284845b52c03c287159b49bd995
R3dWulf <theakaiokami@gmail.com>
2018-05-07 20:40:59 -0400

    Can register user from registation page

--- DatingApp.SPA/src/app/_services/auth.service.ts
+++ DatingApp.SPA/src/app/_services/auth.service.ts
@@ -41,8 +41,10 @@ export class AuthService {
             .catch(this.handleError);
     }
 
-    register(model: any) {
-        return this.http.post(this.baseUrl + 'register', model, this.requestOptions()).catch(this.handleError);
+    register(user: User) {
+        return this.http
+        .post(this.baseUrl + 'register', user, this.requestOptions())
+        .catch(this.handleError);
     }
 
     loggedIn() {
--- DatingApp.SPA/src/app/register/register.component.ts
+++ DatingApp.SPA/src/app/register/register.component.ts
@@ -3,6 +3,8 @@ import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { BsDatepickerConfig } from 'ngx-bootstrap';
+import { User } from '../_models/User';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -11,14 +13,15 @@ import { BsDatepickerConfig } from 'ngx-bootstrap';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  user: User;
   registerForm: FormGroup;
   bsConfig: Partial<BsDatepickerConfig>;
 
   constructor(
     private authService: AuthService,
     private alertify: AlertifyService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -51,14 +54,18 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    // this.authService.register(this.model).subscribe(() => {
-    //   /*console.log('Registration successful');*/
-    //   this.alertify.success('Registration was a success');
-    // }, error => {
-    //   /*console.log(error);*/
-    //   this.alertify.error(error);
-    // });
-    console.log(this.registerForm.value);
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      this.authService.register(this.user).subscribe( () => {
+        this.alertify.success('Registration was successful!');
+      }, error => {
+        this.alertify.error(error);
+      }, () => {
+        this.authService.login(this.user).subscribe( () => {
+          this.router.navigate(['/members']);
+        });
+      });
+    }
   }
 
   cancel() {
