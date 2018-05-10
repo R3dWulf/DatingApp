5043fb8c749113fb6550bfd60d78c9f28ba6584b
R3dWulf <theakaiokami@gmail.com>
2018-05-10 13:11:44 -0400

    Setup pagination on server side

--- DatingAPP.API/Data/IDatingRepository.cs
+++ DatingAPP.API/Data/IDatingRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using DatingAPP.API.Helpers;
 using DatingAPP.API.Models;
 using DatingAPP.API.Modules;
 
@@ -10,7 +11,7 @@ namespace DatingAPP.API.Data
          void Add<T>(T entity) where T: class;
          void Delete<T>(T entity) where T: class;
          Task<bool> SaveAll(); 
-         Task<IEnumerable<User>> GetUsers();
+         Task<PagedList<User>> GetUsers(UserParams userParams);
          Task<User> GetUser(int id);
          Task<Photo> GetPhoto(int id);
          Task<Photo> GetMainPhotoForUser(int userId);
