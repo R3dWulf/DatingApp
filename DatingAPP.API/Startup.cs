119865d2cc98d77482388fc69702a1fb7ff4c5e5
R3dWulf <theakaiokami@gmail.com>
2018-05-22 10:56:00 -0400

    Using NG Build to run of kestrel server

--- DatingAPP.API/Startup.cs
+++ DatingAPP.API/Startup.cs
@@ -88,7 +88,14 @@ namespace DatingApp.API
             //seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
             app.UseAuthentication();
-            app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc( routes => {
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Fallback", action = "Index" }
+                );
+            });
         }
     }
 }
